b0bb6d089a0c2621a4785a7e0d8cff519f0f0e44
bobr-10 <bobr_k132@mail.ru>
2023-02-02 09:15:51 +0300

    Added subtraction

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -26,20 +26,20 @@ namespace calculator_c_sharp
                         res = num1 + num2;
                         Console.WriteLine("Addition:" + res);
                         break;
-                    //case "-":
-                    //    res = num1 - num2;
-                    //    Console.WriteLine("Subtraction:" + res);
-                    //    break;
-                    //case "*":
-                    //    res = num1 * num2;
-                    //    Console.WriteLine("Multiplication:" + res);
-                    //    break;
-                    //case "/":
-                    //    res = num1 / num2;
-                    //    Console.WriteLine("Division:" + res);
-                    //    break;
-                    //default:
-                    //    Console.WriteLine("Wrong input");
+                    case "-":
+                        res = num1 - num2;
+                        Console.WriteLine("Subtraction:" + res);
+                        break;
+                    case "*":
+                        //    res = num1 * num2;
+                        //    Console.WriteLine("Multiplication:" + res);
+                        //    break;
+                        //case "/":
+                        //    res = num1 / num2;
+                        //    Console.WriteLine("Division:" + res);
+                        //    break;
+                        //default:
+                        //    Console.WriteLine("Wrong input");
                         break;
                 }
                 Console.ReadLine();
